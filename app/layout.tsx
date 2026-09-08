c8ec9a9684bfd3b2b57a2e4b2ed93ce44997c461
paulajessica-dev <jessicafldasilva@gmail.com>
2026-09-08 10:17:39 -0300

    add metada

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
+
+import type { Metadata } from "next";
 import "./globals.css";
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+
+export const metadata: Metadata = {
+  title: "Paula Jessica Silva | Portfolio",
+  description: "A BI Analyst's portfolio showcasing web development projects built while learning Next.js and React.",
+};
 
 export default function RootLayout({
       children,
@@ -16,4 +23,4 @@ export default function RootLayout({
           </body>
         </html>
       );
-}
+};
